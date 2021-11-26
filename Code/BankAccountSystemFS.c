c90864ef7eba282fd7938e85b52bba4183628a1e
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2021-11-26 10:12:37 +0530

    Updated BankAccountSystemFS.c

--- Code/BankAccountSystemFS.c
+++ Code/BankAccountSystemFS.c
@@ -91,3 +91,100 @@ int main(){
 		getch();
 	}
 }
+
+void login(void){
+	system("cls");
+
+	char username[50];
+	char password[50];
+
+	int i, j, k;
+	char ch;
+	FILE *fp, *fu;
+	struct pass u1;
+	struct userpass u2;
+
+	fp = fopen("username.txt",
+			"rb");
+
+	if (fp == NULL) {
+		printf("ERROR IN OPENING FILE");
+	}
+	gotoxy(34, 2);
+	printf(" ACCOUNT LOGIN ");
+	gotoxy(7, 5);
+	printf("***********************************************"
+		"********************************");
+
+	gotoxy(35, 10);
+	printf("==== LOG IN ====");
+
+	gotoxy(35, 12);
+	printf("USERNAME.. ");
+	scanf("%s", &username);
+
+	gotoxy(35, 14);
+	printf("PASSWORD..");
+
+	for (i = 0; i < 50; i++) {
+		ch = getch();
+		if (ch != 13) {
+			password[i] = ch;
+			ch = '*';
+			printf("%c", ch);
+		}
+
+		else
+			break;
+	}
+
+	while (fread(&u1, sizeof(u1), 1, fp)) {
+		if (strcmp(username, u1.username)== 0) {
+			loginsu();
+			display(username);
+		}
+	}
+
+	fclose(fp);
+}
+
+void loginsu(void){
+	int i;
+	FILE* fp;
+	struct pass u1;
+	system("cls");
+	printf("Fetching account details.....\n");
+	for (i = 0; i < 20000; i++) {
+		i++;
+		i--;
+	}
+
+	gotoxy(30, 10);
+	printf("LOGIN SUCCESSFUL....");
+	gotoxy(0, 20);
+	printf("Press enter to continue");
+
+	getch();
+}
+
+void logout(void){
+	int i, j;
+	system("cls");
+	printf("please wait, logging out");
+
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 25000000; j++) {
+			i++;
+			i--;
+		}
+		printf(".");
+	}
+
+	gotoxy(30, 10);
+	printf("Sign out successfully..\n");
+
+	gotoxy(0, 20);
+	printf("press any key to continue..");
+
+	getch();
+}
