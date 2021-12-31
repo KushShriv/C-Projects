a83e9226d35c37001fe4fa25885ec9121930e048
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2021-12-31 14:33:03 +0530

    Updated SnakeGame.c

--- Code/SnakeGame.c
+++ Code/SnakeGame.c
@@ -64,3 +64,92 @@ int main(){
     Move(); 
     return 0;
 }
+
+void Move(){
+    int a,i;
+    do{
+        Food();
+        fflush(stdin);
+        len=0;
+        
+        for(i=0; i<30; i++){
+            body[i].x=0;
+            body[i].y=0;
+            if(i==length)
+                break;
+        }
+        Delay(length);
+        Boarder();
+        if(head.direction==RIGHT)
+            Right();
+        else if(head.direction==LEFT)
+            Left();
+        else if(head.direction==DOWN)
+            Down();
+        else if(head.direction==UP)
+            Up();
+        ExitGame();
+    }
+    while(!kbhit());
+
+    a=getch();
+    if(a==27){
+        system("cls");
+        exit(0);
+    }
+    key=getch();
+
+    if((key==RIGHT&&head.direction!=LEFT&&head.direction!=RIGHT)
+        ||(key==LEFT&&head.direction!=RIGHT&&head.direction!=LEFT)
+        ||(key==UP&&head.direction!=DOWN&&head.direction!=UP)
+        ||(key==DOWN&&head.direction!=UP&&head.direction!=DOWN))
+    {
+        bend_no++;
+        bend[bend_no]=head;
+        head.direction=key;
+        if(key==UP)
+            head.y--;
+        if(key==DOWN)
+            head.y++;
+        if(key==RIGHT)
+            head.x++;
+        if(key==LEFT)
+            head.x--;
+        Move();
+    }
+    else if(key==27){
+        system("cls");
+        exit(0);
+    }
+    else{
+        printf("\a");
+        Move();
+    }
+}
+
+void gotoxy(int x, int y){
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void GotoXY(int x, int y){
+    HANDLE a;
+    COORD b;
+    fflush(stdout);
+    b.X = x;
+    b.Y = y;
+    a = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(a,b);
+}
+void load(){
+    int row,col,r,c,q;
+    gotoxy(36,14);
+    printf("loading...");
+    gotoxy(30,15);
+    for(r=1; r<=20; r++){
+        for(q=0; q<=100000000; q++);
+        printf("%c",177);
+    }
+    getch();
+}
