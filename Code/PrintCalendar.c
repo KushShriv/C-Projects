5f44b6266eada91d218f12d97b27045222929526
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2022-01-10 19:11:19 +0530

    Completed PrintCalendar.c

--- Code/PrintCalendar.c
+++ Code/PrintCalendar.c
@@ -1,5 +1,11 @@
 #include <stdio.h> 
 
+int dayNumber(int day, int month, int year) { 
+	static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
+	year -= month < 3; 
+	return (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7; 
+} 
+
 char* getMonthName(int monthNumber) { 
 	char* month; 
 
@@ -60,6 +66,34 @@ int numberOfDays(int monthNumber, int year) {
 	if (monthNumber == 9) return (31); 
 	if (monthNumber == 10) return (30); 
 	if (monthNumber == 11) return (31); 
+} 
+
+void printCalendar(int year) { 
+	printf("	 Calendar - %d\n\n", year); 
+	int days; 
+	int current = dayNumber(1, 1, year); 
+
+	for (int i = 0; i < 12; i++) { 
+		days = numberOfDays(i, year); 
+
+		printf("\n ------------%s-------------\n", getMonthName(i)); 
+		printf("  Sun Mon Tue Wed Thu Fri Sat\n"); 
+
+		int k; 
+		for (k = 0; k < current; k++) 
+			printf("\t\t "); 
+		for (int j = 1; j <= days; j++) { 
+			printf("%5d", j); 
+			if (++k > 6) { 
+				k = 0; 
+				printf("\n"); 
+			} 
+		} 
+		if (k) 
+			printf("\n"); 
+		current = k; 
+	} 
+	return; 
 } 
 
 int main() { 
