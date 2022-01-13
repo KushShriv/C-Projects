99749a1a5ca97a7bf28c73ed1c5b2cdf122f3a3a
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2022-01-13 10:10:20 +0530

    Created TicTacToe.c

---
+++ Code/TicTacToe.c
@@ -0,0 +1,16 @@
+#include <stdbool.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#define COMPUTER 1 
+#define HUMAN 2 
+#define SIDE 3 
+#define COMPUTERMOVE 'O' 
+#define HUMANMOVE 'X' 
+
+char player = 'x', opponent = 'o'; 
+
+int main() { 
+	playTicTacToe(COMPUTER); 
+	return 0; 
+}
