4fd29c120022687f5ba7b955d5c10aa99ca83972
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2022-01-25 10:02:53 +0530

    Updated 2048.c

--- Code/2048.c
+++ Code/2048.c
@@ -66,5 +66,112 @@ int main(){
     fclose(ptr); 
     addrandomno(); 
     print(); 
+
+    while (1) { 
+        if (score > highscore) { 
+            ptr = fopen("highscore.txt", "w"); 
+            fprintf(ptr, "%d", score); 
+            fclose(ptr); 
+        } 
+        choise = getchar(); 
+        while (getchar() != '\n'); 
+
+        if (choise == 'D' || choise == 'd') { 
+            count++; 
+            ch++; 
+            createprev(p); 
+            for (int i = 0; i < 4; i++) { 
+                for (int j = 3; j >= 0; j--) { 
+                    c[j] = arr[i][j]; 
+                } 
+                rupdate(); 
+                for (int j = 0; j < 4; j++) { 
+                    arr[i][j] = c[j]; 
+                } 
+            } 
+        } 
+        else if (choise == 'A' || choise == 'a') { 
+            count++; 
+            ch++; 
+            createprev(p); 
+            for (int i = 0; i < 4; i++) { 
+                for (int j = 0; j < 4; j++) { 
+                    c[j] = arr[i][j]; 
+                } 
+                rupdate(); 
+                for (int j = 0; j < 4; j++) { 
+                    arr[i][j] = c[j]; 
+                } 
+            } 
+        } 
+        else if (choise == 'S' || choise == 's') { 
+            count++; 
+            ch++; 
+            createprev(p); 
+            for (int i = 0; i < 4; i++) { 
+                for (int j = 0; j < 4; j++) { 
+                    c[j] = arr[j][i]; 
+                } 
+                rupdate(); 
+                for (int j = 0; j < 4; j++) { 
+                    arr[j][i] = c[j]; 
+                } 
+            } 
+        } 
+        else if (choise == 'W' || choise == 'w') { 
+            count++; 
+            ch++; 
+            createprev(p); 
+            for (int i = 0; i < 4; i++) { 
+                for (int j = 3; j >= 0; j--) { 
+                    c[j] = arr[j][i]; 
+                } 
+                rupdate(); 
+                for (int j = 0; j < 4; j++) { 
+                    arr[j][i] = c[j]; 
+                } 
+            } 
+        } 
+        else if (choise == 'P' || choise == 'p') { 
+            ch++; 
+            system("cls"); 
+            updatearrtoprev(p); 
+        } 
+        else if (choise == 'R' || choise == 'r') { 
+            ch++; 
+            printf("\n******************DO YOU WANT TO RESTART -> Y or N ******************\n"); 
+            reschk = getchar(); 
+            while (getchar() != '\n'); 
+
+            if (reschk == 'Y' || reschk == 'y') { 
+                resetgame(); 
+            } 
+            else { 
+                system("cls"); 
+                print(); 
+            } 
+        } 
+        else if (choise == 'U' || choise == 'u') { 
+            printf("\n\t\t\t\t\tYOUR TOTAL SCORE=%d\n", score); 
+            printf("\t\t\t\t\t---------------------------------\n"); 
+            printf("\t\t\t\t\tTOTAL STEPS MOVED=%d\n", ch); 
+            printf("\t\t\t\t\t---------------------------------\n"); 
+            printf("\t\t\t\t\tQUITTING"); 
+            for (int i = 0; i < 5; i++) { 
+                Sleep(500); 
+                printf("."); 
+            } 
+            exit(0); 
+        } 
+        else { 
+            printf("\nENTER A VALID CHOICE\n"); 
+            system("cls"); 
+            print(); 
+            continue; 
+        }
+        addrandomno(); 
+        system("cls"); 
+        print(); 
+    } 
     return 0; 
 } 
